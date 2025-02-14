87dde848d9d48ff52bdbf963cb5787081fe7fd0a
nodar-miminoshvili <miminoshvilinodar@gmail.com>
2025-02-15 02:22:50 +0400

    Set CORS headers

--- back/public/index.php
+++ back/public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
